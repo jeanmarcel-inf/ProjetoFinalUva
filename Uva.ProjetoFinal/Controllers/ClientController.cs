a492bd5135dec93eacc9845004bada68bbb6b29b
jeanmarcel-inf <jeanmmaia.inf@gmail.com>
2023-05-28 01:02:25 -0300

    AddDeleteAction

--- Uva.ProjetoFinal/Controllers/ClientController.cs
+++ Uva.ProjetoFinal/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Uva.ProjetoFinal.Data;
 using Uva.ProjetoFinal.Models;
 
@@ -12,11 +13,37 @@ namespace Uva.ProjetoFinal.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            IEnumerable<ClientModel> clients = _context.Clients;
+            var tables = new ListClientsViewModel()
+            {
+                Clients = _context.Clients.ToList(),
+                Addresses = _context.Addresses.ToList(),
+                BrowsingHistory = _context.BrowsingHistory.ToList(),
+                Emails = _context.Emails.ToList(),
+            };
 
-            return View(clients);
+            return View(tables);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var client = _context.Clients.Where(c => c.Id == id).FirstOrDefault();
+            var address = _context.Addresses.Where(a => a.ClientId == id).FirstOrDefault();
+            var browsingHistory = _context.BrowsingHistory.Where(bh => bh.ClientId == id).FirstOrDefault();
+            var email = _context.Emails.Where(e => e.ClientId == id).FirstOrDefault();
+
+            _context.Clients.Remove(client);
+            _context.Addresses.Remove(address);
+            _context.BrowsingHistory.Remove(browsingHistory);
+            _context.Emails.Remove(email);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
